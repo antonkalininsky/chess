0969ee39c3efd3c5669227ceab438227e36bf305
anton <chadik.free@gmail.com>
2022-03-03 22:21:23 +0700

    mainwindow: - added and placed play field, start button and logs field

--- chess/mainwindow.cpp
+++ chess/mainwindow.cpp
@@ -1,11 +1,38 @@
 #include "mainwindow.h"
+#include <QGroupBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QDockWidget>
+#include <QSizePolicy>
+#include <QWidget>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
+    // size of window
+    resize(800, 660);
+    setFixedSize(800, 660);
+
+    // setting board area
+    board = new QGraphicsScene();
+    viewBoard = new QGraphicsView(board);
+    viewBoard->setFixedSize(640, 640);
+
+    // setting info area
+    logs = new QTextEdit();
+    logs->setReadOnly(true);
+    button = new QPushButton("START");
+    QVBoxLayout *sideBar = new QVBoxLayout();
+    sideBar->addWidget(button);
+    sideBar->addWidget(logs);
+
+    // placing
+    QHBoxLayout *centralLayout = new QHBoxLayout();
+    centralLayout->addWidget(viewBoard);
+    centralLayout->insertLayout(-1, sideBar);
+    QGroupBox *centralWidget = new QGroupBox();
+    centralWidget->setLayout(centralLayout);
+    setCentralWidget(centralWidget);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
 
 }
--- chess/mainwindow.h
+++ chess/mainwindow.h
@@ -2,14 +2,23 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
-
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+private:
+    QPushButton *button;
+    QTextEdit *logs;
+    QGraphicsScene *board;
+    QGraphicsView *viewBoard;
+    QGraphicsView *viewSidebar;
+
 };
 
 #endif // MAINWINDOW_H
